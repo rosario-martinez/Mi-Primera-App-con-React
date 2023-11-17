e19f35610bf2e08e86949c676307a58f66fa4431
Rosario Martinez <angela_ajd@hotmail.com>
2023-11-17 15:35:25 -0300

    corregido el codigo con el codigo nuevo

--- react-curso/aprendiendo_react/src/index.js
+++ react-curso/aprendiendo_react/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const JSX = <h1>Hola Rosario Bienvenida</h1>
+// Código nuevo
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hola Rosario Bienvenida</h1>
+    {JSX}
   </React.StrictMode>
 );
