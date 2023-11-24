1cc5e4e5fd89800ffea085ac4dcdeef2735398aa
Rosario Martinez <angela_ajd@hotmail.com>
2023-11-24 16:28:09 -0300

    formlarios en react parte 2

--- react-curso/aprendiendo_react/src/componentes/FormularioInicioSesion.js
+++ react-curso/aprendiendo_react/src/componentes/FormularioInicioSesion.js
@@ -4,6 +4,23 @@ const FormularioInicioSesion = () => {
     const[usuario, cambiarUsuario] = useState ('');
     const [password, cambiarPassword] = useState('');
 
+    //const onChangeUsuario = (evento) => {
+        //console.log(evento);
+        //cambiarUsuario(evento.target.value);
+    //}
+
+    //const onChangePassword = (evento) => {
+      
+     // cambiarPassword(evento.target.value);
+   // };
+
+    const onChange = (evento) => {
+        if(evento.target.name === 'suario'){
+            cambiarUsuario(evento.target.value);
+        }else if (evento.target.name === 'password'){
+            cambiarPassword(evento.target.vale);
+        }
+    }
 
     return (
       <form action="">
@@ -11,19 +28,23 @@ const FormularioInicioSesion = () => {
         <p>Constrase*a: {password}</p>
         <div>
           <label htmlFor="usuario">Usuario</label>
-          <input 
-          type="text" 
-          name="usuario" 
-          id="usuario" 
-          value={usuario}
-          onChange={() => {
-            console.log('El input cambio')
-          }}
+          <input
+            type="text"
+            name="usuario"
+            id="usuario"
+            value={usuario}
+            onChange= {onChange}
           />
         </div>
         <div>
           <label htmlFor="password">Contrase*a</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
         </div>
         <button type="submit">Iniciar Sesion</button>
       </form>
