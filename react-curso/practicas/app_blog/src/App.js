233ead5c319394746862d7b7d9f20d7a75d371f8
Rosario Martinez <angela_ajd@hotmail.com>
2023-12-28 12:16:38 -0300

    separando mi blog personal por carpetas diferente a traves de react-router (aplicando NavLink)

--- react-curso/practicas/app_blog/src/App.js
+++ react-curso/practicas/app_blog/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>Mi Blog Personal</h1>
+          <nav>
+            <NavLink to="/">Inicio </NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/acerca-de">Acerca de </NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h2>Pagina de Inicio</h2>
+                  <p>Esta es la pagina principal de nuestro sitio</p>
+                </div>
+              }
+            />
+            <Route
+              path="/blog"
+              element={
+                <div>
+                  <h2>Blog</h2>
+                  <ul>
+                    <li>Articulo #1</li>
+                    <li>Articulo #2</li>
+                    <li>Articulo #3</li>
+                  </ul>
+                </div>
+              }
+            />
+            <Route
+              path="/acerca-de"
+              element={
+                <div>
+                  <h2>Acerca de</h2>
+                  <p>Hola! Mi nombre es Rosario Martinez</p>
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
