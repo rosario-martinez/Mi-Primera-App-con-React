8328183ab55e39c88d9a0a0ab9cb4d2b3d0cf26c
Rosario Martinez <angela_ajd@hotmail.com>
2023-12-28 17:54:09 -0300

    colocando los estilos css en el header

--- react-curso/practicas/app_blog/src/App.js
+++ react-curso/practicas/app_blog/src/App.js
@@ -4,14 +4,17 @@ import Header from "./componentes/Header";
 import Inicio from "./componentes/Inicio";
 import Blog from "./componentes/Blog";
 import AcercaDe from "./componentes/AcercaDe";
+import styled from "styled-components";
+
 
 const App = () => {
   return (
     <BrowserRouter>
+    <ContenedorPrincipal>
       <div>
         <Header/>
         
-        <main>
+        <Main>
           <Routes>
             
             <Route path="/"element={<Inicio/>}/>
@@ -19,10 +22,26 @@ const App = () => {
             <Route path="/acerca-de" element={<AcercaDe/> }
             />
           </Routes>
-        </main>
+        </Main>
       </div>
+    </ContenedorPrincipal>
     </BrowserRouter>
   );
-};
+}  
+
+ const ContenedorPrincipal = styled.div`
+    padding: 40px;
+    width: 90%;
+    max-width: 700px;
+ 
+ `;
+
+ const Main = styled.main`
+  background: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+  
+ `;
 
 export default App;
